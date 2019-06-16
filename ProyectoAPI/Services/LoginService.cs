12f6fb44dac36e6c62411fb5e2733ad1e0e7e3b9
LucasRanoni <lucasunlam@gmail.com>
2019-06-16 01:13:04 -0300

    login

--- ProyectoAPI/Services/LoginService.cs
+++ ProyectoAPI/Services/LoginService.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using ProyectoAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ProyectoAPI;
 
 namespace ProyectoAPI.Services
 {
     public class LoginService
     {
+        todaviasirveDBEntities contexto = new todaviasirveDBEntities();
+
+        public bool verificarDatos(Usuario usu)
+        {
+            Usuario usuarioEncontrado = contexto.Usuario.Where(u => u.email.Equals(usu.email) && u.pass.Equals(usu.pass)).FirstOrDefault();
+            
+            if(usuarioEncontrado != null) {
+                HttpContext.Current.Session["idUsuario"] = usuarioEncontrado.id;
+                HttpContext.Current.Session["email"] = usuarioEncontrado.email;
+
+                return true;
+            }
+
+            return false;
+        }
     }
 }
