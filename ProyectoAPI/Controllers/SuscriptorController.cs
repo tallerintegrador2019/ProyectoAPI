86de0959c1362be583614c5cf4438126d332feaa
Dar1us <darius05g4@hotmail.com>
2019-06-18 19:04:56 -0300

    Cambios en Suscriptor

---
+++ ProyectoAPI/Controllers/SuscriptorController.cs
@@ -0,0 +1,37 @@
+﻿using ProyectoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoAPI.Controllers
+{
+    public class SuscriptorController : Controller
+    {
+
+        private todaviasirveDBEntities db = new todaviasirveDBEntities();
+
+        // GET: Suscriptor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Suscriptor suscr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suscriptor.Add(suscr);
+                db.SaveChanges();
+                return RedirectToAction("Publicacion");
+            }
+
+            return View(suscr);
+        }
+    }
+
+    
+}
