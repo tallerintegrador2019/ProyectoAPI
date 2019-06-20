03e277cfa9dfb8c69237bfc36463f8d36c050fcf
LucasRanoni <lucasunlam@gmail.com>
2019-06-20 01:12:20 -0300

    Con Materialize

--- ProyectoAPI/App_Start/BundleConfig.cs
+++ ProyectoAPI/App_Start/BundleConfig.cs
@@ -5,6 +5,7 @@ namespace ProyectoAPI
 {
     public class BundleConfig
     {
+        // Con materialize
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
