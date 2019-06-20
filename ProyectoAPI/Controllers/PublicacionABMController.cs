6c1117b5bbd80fa829701b1923bcbb92e554d8be
rober <rcfernandez@outlook.com.ar>
2019-06-20 11:15:27 -0300

    agregado cruds

---
+++ ProyectoAPI/Controllers/PublicacionABMController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoAPI.Models;
+
+namespace ProyectoAPI.Controllers
+{
+    public class PublicacionABMController : Controller
+    {
+        private todaviasirveDBEntities db = new todaviasirveDBEntities();
+
+        // GET: PublicacionABM
+        public ActionResult Index()
+        {
+            return View(db.Publicacion.ToList());
+        }
+
+        // GET: PublicacionABM/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publicacion publicacion = db.Publicacion.Find(id);
+            if (publicacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publicacion);
+        }
+
+        // GET: PublicacionABM/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PublicacionABM/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,titulo,subtitulo,descripcion,fechaSubida,imagenPortada")] Publicacion publicacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Publicacion.Add(publicacion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(publicacion);
+        }
+
+        // GET: PublicacionABM/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publicacion publicacion = db.Publicacion.Find(id);
+            if (publicacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publicacion);
+        }
+
+        // POST: PublicacionABM/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,titulo,subtitulo,descripcion,fechaSubida,imagenPortada")] Publicacion publicacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(publicacion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(publicacion);
+        }
+
+        // GET: PublicacionABM/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Publicacion publicacion = db.Publicacion.Find(id);
+            if (publicacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publicacion);
+        }
+
+        // POST: PublicacionABM/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Publicacion publicacion = db.Publicacion.Find(id);
+            db.Publicacion.Remove(publicacion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProyectoAPI.Models;
+
+namespace ProyectoAPI.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        private todaviasirveDBEntities db = new todaviasirveDBEntities();
+
+        // GET: api/Usuario
+        public IQueryable<Usuario> GetUsuario()
+        {
+            return db.Usuario;
+        }
+
+        // GET: api/Usuario/5
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult GetUsuario(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        // PUT: api/Usuario/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUsuario(int id, Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuario.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Usuario
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult PostUsuario(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Usuario.Add(usuario);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = usuario.id }, usuario);
+        }
+
+        // DELETE: api/Usuario/5
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult DeleteUsuario(int id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+
+            return Ok(usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return db.Usuario.Count(e => e.id == id) > 0;
+        }
+    }
+}
