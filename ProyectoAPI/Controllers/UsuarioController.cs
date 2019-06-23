a8658b30b71b1668555a30aef45b20c0ba17bf48
rober <rcfernandez@outlook.com.ar>
2019-06-23 11:25:51 -0300

    agregado subir foto a api usuario

--- ProyectoAPI/Controllers/UsuarioController.cs
+++ ProyectoAPI/Controllers/UsuarioController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Cors;
@@ -74,20 +76,105 @@ namespace ProyectoAPI.Controllers
             return StatusCode(HttpStatusCode.NoContent);
         }
 
+
+
+
+
+        //// POST: api/Usuario
+        //[ResponseType(typeof(Usuario))]
+        //public IHttpActionResult PostUsuario(Usuario usuario)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Usuario.Add(usuario);
+        //    db.SaveChanges();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = usuario.id }, usuario);
+        //}
+
+
         // POST: api/Usuario
         [ResponseType(typeof(Usuario))]
-        public IHttpActionResult PostUsuario(Usuario usuario)
+        public async Task<IHttpActionResult> PostUsuario()
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            db.Usuario.Add(usuario);
-            db.SaveChanges();
+            Usuario usu = new Usuario();
+            var request = HttpContext.Current.Request;
 
-            return CreatedAtRoute("DefaultApi", new { id = usuario.id }, usuario);
+            if (Request.Content.IsMimeMultipartContent())
+            {
+                string root1 = HttpContext.Current.Server.MapPath("~/Content/Images");
+                var provider = new MultipartFormDataStreamProvider(root1);
+                // Read the form data.
+                await Request.Content.ReadAsMultipartAsync(provider);
+                // This illustrates how to get the file names.
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    Trace.WriteLine("Server file path: " + file.LocalFileName);
+
+                }
+                foreach (var key in provider.FormData.AllKeys)
+                {
+                    if (!key.Equals("__RequestVerificationToken"))
+                    {
+                        switch (key)
+                        {
+                            case "nombre":
+                                usu.nombre = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "apellido":
+                                usu.apellido = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "email":
+                                usu.email = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "pass":
+                                usu.pass = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "username":
+                                usu.username = provider.FormData.GetValues(key)[0];
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+
+                }
+
+                if (request.Files.Count > 0)
+                {
+                    var imagen = request.Files[0];
+                    var postedFile = request.Files.Get("file");
+                    string root = Path.Combine(HttpContext.Current.Server.MapPath("~/Content/Images"), imagen.FileName);
+                    //root = root + "/" + imagen.FileName;
+                    imagen.SaveAs(root);
+                    usu.imagen = imagen.FileName;
+                }
+
+                db.Usuario.Add(usu);
+                db.SaveChanges();
+
+                return Ok(HttpStatusCode.OK);
+
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = usu.id }, usu);
         }
+
+
+
+
+
+
 
         // DELETE: api/Usuario/5
         [ResponseType(typeof(Usuario))]
