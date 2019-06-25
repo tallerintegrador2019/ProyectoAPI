34cf8ebb8334e4c5db48dbe7336ad075bfaae8ce
LucasRanoni <lucasunlam@gmail.com>
2019-06-25 10:13:18 -0300

    publicacion post api

--- ProyectoAPI/Controllers/PublicacionController.cs
+++ ProyectoAPI/Controllers/PublicacionController.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -72,20 +73,175 @@ namespace ProyectoAPI.Controllers
             return StatusCode(HttpStatusCode.NoContent);
         }
 
+       
+
         // POST: api/Publicacion
+        //[ResponseType(typeof(Publicacion))]
+        //public IHttpActionResult PostPublicacion(Publicacion publicacion)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Publicacion.Add(publicacion);
+        //    db.SaveChanges();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = publicacion.id }, publicacion);
+        //}
+
+
+        // POST: api/Publicacion                    Nuevo Post
+        [System.Web.Http.HttpPost]
         [ResponseType(typeof(Publicacion))]
-        public IHttpActionResult PostPublicacion(Publicacion publicacion)
+        public async Task<IHttpActionResult> PostPublicacionAsync()
         {
-            if (!ModelState.IsValid)
+            Publicacion publicacion = new Publicacion();
+            Paso paso1 = new Paso();
+            Paso paso2 = new Paso();
+            Paso paso3 = new Paso();
+            Paso paso4 = new Paso();
+            Paso paso5 = new Paso();
+
+            var request = HttpContext.Current.Request;
+            bool imagenDePublicacion = true;
+
+            if (Request.Content.IsMimeMultipartContent())
             {
-                return BadRequest(ModelState);
+                string root1 = HttpContext.Current.Server.MapPath("~/Content/Images");
+                var provider = new MultipartFormDataStreamProvider(root1);
+                // Read the form data.
+                await Request.Content.ReadAsMultipartAsync(provider);
+                // This illustrates how to get the file names.
+                //foreach (MultipartFileData file in provider.FileData)
+                //{
+                //    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                //    Trace.WriteLine("Server file path: " + file.LocalFileName);
+                //}
+                foreach (var key in provider.FormData.AllKeys)
+                {
+                    if (!key.Equals("__RequestVerificationToken"))
+                    {
+                        switch (key)
+                        {
+                            case "titulo":
+                                publicacion.titulo = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "subtitulo":
+                                publicacion.subtitulo = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "descripcion":
+                                publicacion.descripcion = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "descripcion1":
+                                paso1.descripcion = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "descripcion2":
+                                paso2.descripcion = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "descripcion3":
+                                paso3.descripcion = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "descripcion4":
+                                paso4.descripcion = provider.FormData.GetValues(key)[0];
+                                break;
+                            case "descripcion5":
+                                paso5.descripcion = provider.FormData.GetValues(key)[0];
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+                for (var j=0; j < request.Files.Count; j++)
+                {
+                    var archivo = request.Files[j];
+                    var cantidadArchivos = request.Files.Count;
+                    var fileName = "";
+                    var imagenlocal = "";
+                    switch (j+1)
+                    {
+                        case 1:
+                            var nombre = Path.GetFileName(archivo.FileName);
+                            string root = Path.Combine(root1, nombre);
+                            archivo.SaveAs(root);
+                            publicacion.imagenPortada = nombre;
+                            db.Publicacion.Add(publicacion);
+                            db.SaveChanges();
+                            break;
+                        case 2:
+                            fileName = Path.GetFileName(archivo.FileName);
+                            imagenlocal = Path.Combine(root1, fileName);
+                            archivo.SaveAs(imagenlocal);
+                            paso1.idPublicacion = publicacion.id;
+                            paso1.imagen = fileName;
+                            db.Paso.Add(paso1);
+                            db.SaveChanges();
+                            break;
+                        case 3:
+                            fileName = Path.GetFileName(archivo.FileName);
+                            imagenlocal = Path.Combine(root1, fileName);
+                            archivo.SaveAs(imagenlocal);
+                            paso2.idPublicacion = publicacion.id;
+                            paso2.imagen = fileName;
+                            db.Paso.Add(paso2);
+                            db.SaveChanges();
+                            break;
+                        case 4:
+                            fileName = Path.GetFileName(archivo.FileName);
+                            imagenlocal = Path.Combine(root1, fileName);
+                            archivo.SaveAs(imagenlocal);
+                            paso3.idPublicacion = publicacion.id;
+                            paso3.imagen = fileName;
+                            db.Paso.Add(paso3);
+                            db.SaveChanges();
+                            break;
+                        case 5:
+                            fileName = Path.GetFileName(archivo.FileName);
+                            imagenlocal = Path.Combine(root1, fileName);
+                            archivo.SaveAs(imagenlocal);
+                            paso4.idPublicacion = publicacion.id;
+                            paso4.imagen = fileName;
+                            db.Paso.Add(paso4);
+                            db.SaveChanges();
+                            break;
+                        case 6:
+                            fileName = Path.GetFileName(archivo.FileName);
+                            imagenlocal = Path.Combine(root1, fileName);
+                            archivo.SaveAs(imagenlocal);
+                            paso5.idPublicacion = publicacion.id;
+                            paso5.imagen = fileName;
+                            db.Paso.Add(paso5);
+                            db.SaveChanges();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+
+                //if (request.Files.Count > 0)
+                //{
+                //    var imagen = request.Files[0];
+                //    var postedFile = request.Files.Get("file");
+                //    string root = Path.Combine(HttpContext.Current.Server.MapPath("~/Content/Images"), imagen.FileName);
+                //    //root = root + "/" + imagen.FileName;
+                //    imagen.SaveAs(root);
+                //    publicacion.imagen = imagen.FileName;
+
+
+                //}
+
+                return Ok(HttpStatusCode.OK);
+
+            }
+            else {
+                return BadRequest();
             }
 
-            db.Publicacion.Add(publicacion);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = publicacion.id }, publicacion);
         }
+
 
         // DELETE: api/Publicacion/5
         [ResponseType(typeof(Publicacion))]
