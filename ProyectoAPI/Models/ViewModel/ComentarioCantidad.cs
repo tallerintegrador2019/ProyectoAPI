5e6cc2326753eb1003d3032312ad2ce6cf58c13c
LucasRanoni <lucasunlam@gmail.com>
2019-07-11 11:52:11 -0300

    cambios

---
+++ ProyectoAPI/Models/ViewModel/ComentarioCantidad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoAPI.Models.ViewModel
+{
+    public class ComentarioCantidad
+    {
+        public List<ComentarioUsuario> comentarioUsuarios { get; set; }
+        public string cantidad { get; set; }
+    }
+}
